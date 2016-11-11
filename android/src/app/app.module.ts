ab17a924ab7e782a468dd802770775ec962921ea
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-11-11 23:02:20 +0100

    Routes and landing page added

--- android/src/app/app.module.ts
+++ android/src/app/app.module.ts
@@ -2,19 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import  { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AndroidMdlDirective } from './mdl.directive';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AndroidMdlDirective
+    AndroidMdlDirective,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent  },
+      { path: 'home', redirectTo: '/' },
+      { path: '**', component: HomeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
