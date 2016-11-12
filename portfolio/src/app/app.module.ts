1dffec99505b1cfa11536d5baa9fce1ff8416d75
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-11-12 16:00:18 +0100

    Example blog post subpage'

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { BlogComponent } from './blog/blog.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { ExampleComponent } from './example/example.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ContactComponent } from './contact/contact.component';
     PortfolioComponent,
     BlogComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    ExampleComponent
   ],
   imports: [
     BrowserModule,
@@ -25,6 +27,7 @@ import { ContactComponent } from './contact/contact.component';
     RouterModule.forRoot([
       { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
       { path: 'portfolio', component: PortfolioComponent },
+      { path: 'portfolio/example/:id', component: ExampleComponent },
       { path: 'blog', component: BlogComponent },
       { path: 'about', component: AboutComponent },
       { path: 'contact', component: ContactComponent },
