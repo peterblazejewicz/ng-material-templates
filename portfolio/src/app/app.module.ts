95b9c14e8ca5a19cd528a188202e18e75bbeef57
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-11-12 15:45:18 +0100

    Pages and routes

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,17 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BlogComponent } from './blog/blog.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    BlogComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
+      { path: 'portfolio', component: PortfolioComponent },
+      { path: 'blog', component: BlogComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: '**', redirectTo: 'portfolio' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
