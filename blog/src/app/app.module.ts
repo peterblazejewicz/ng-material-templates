c691b31c3b0ff19d12d683b7ec2e660bad72a5b4
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-11-12 23:19:51 +0100

    Markup, structure and routes

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EntryComponent } from './entry/entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EntryComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'blog', redirectTo: '/' },
+      { path: 'entry', component: EntryComponent },
+      { path: '', component: HomeComponent },
+      { path: '**', redirectTo: '/' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
