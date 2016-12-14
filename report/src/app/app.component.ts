5994def41e42944cbfc98c47869ab08548a764e1
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-12-14 21:42:35 +0100

    MDL dependency and support via NPM

--- report/src/app/app.component.ts
+++ report/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'rprt-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'rprt works!';
+
+     ngAfterViewInit(): void {
+        componentHandler.upgradeAllRegistered();
+    }
 }
